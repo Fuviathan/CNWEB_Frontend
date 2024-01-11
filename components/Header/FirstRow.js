94b3341ae532d9cb466a91453568ad44dd07032c
Fuviathan <FuyuSora02@gmail.com>
2024-01-11 19:21:32 +0700

    create header

---
+++ components/Header/FirstRow.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function FirstRow() {
+  return (
+    <div className='grid items-center grid-cols-2 py-4 text-xs font-normal bg-gray-900 border-b-[1px] border-[#3b4149]'>
+        <div className='px-5 mb-0 text-white'>
+            Free Shipping Over $100 & Free Returns
+        </div>
+        <div className='px-5 mb-0 text-white text-end'>
+            tel: +84 12345678
+        </div>
+    </div>
+  )
+}
---
+++ pages/Abcd.js
@@ -0,0 +1,12 @@
+import Layout from '@/components/Layout'
+import React from 'react'
+
+export default function abcd() {
+    return (
+        <div className=''>
+
+            <Layout />
+        </div>
+
+    )
+}
