2a5b26ed47293930857afe28cff1c06b7e682e57
a39006 <vuvantrung002@gmail.com>
2024-01-23 18:54:08 +0700

    setup provider

--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -1,15 +1,19 @@
-import React from 'react'
-import Header from './Header/Header'
-import Footer from './Footer/Footer'
+import React from "react";
+import Header from "./Header/Header";
+import Footer from "./Footer/Footer";
+import { Provider } from "react-redux";
+import { store } from "@/app/store";
 
 export default function Layout(props) {
   return (
-    <div className='flex flex-col justify-between min-h-screen bg-white h-fit'>
-      <div>
-        <Header />
+    <Provider store={store}>
+      <div className="flex flex-col justify-between min-h-screen bg-white h-fit">
+        <div>
+          <Header />
+        </div>
+        {props.children}
+        <Footer />
       </div>
-      {props.children}
-      <Footer />
-    </div>
-  )
+    </Provider>
+  );
 }
