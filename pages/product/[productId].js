ee80e18ee3868aa3f042e846b27792a34dcf6918
a39006 <vuvantrung002@gmail.com>
2024-01-24 14:29:53 +0700

    add dynamic router

---
+++ pages/product/[productId].js
@@ -0,0 +1,12 @@
+import { Login } from "@mui/icons-material";
+import { BreadCrumb } from "../../components/product/BreadCrumb";
+import ProductDetail from "../../components/product/ProductDetail";
+import Layout from "@/components/Layout/Layout";
+
+export default function Product() {
+  return (
+    <Layout>
+      <ProductDetail></ProductDetail>
+    </Layout>
+  );
+}
