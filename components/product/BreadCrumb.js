b1a0e44ebee2225b14ab026a5f5645c49a323ff9
a39006 <vuvantrung002@gmail.com>
2024-01-28 17:41:16 +0700

    fix breadcumb

--- components/product/BreadCrumb.js
+++ components/product/BreadCrumb.js
@@ -5,10 +5,10 @@ export const BreadCrumb = (props) => {
   return (
     <div className="py-4 mb-0 breadcrumb">
       <p className="pl-8 mb-0 font-mono font-semibold text-left">
-        <Link href={"/"} to="/" className="text-dark">
+        <Link href={"/Abcd"} to="/abcd" className="text-dark">
           Home
         </Link>
-        / {title} "Product"
+        / {title}
       </p>
     </div>
   );
