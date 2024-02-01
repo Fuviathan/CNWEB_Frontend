7bf938a804eecb0b2795304664db582a7a8480ef
Fuviathan <FuyuSora02@gmail.com>
2024-02-01 21:27:29 +0700

    add animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,7 +15,21 @@ module.exports = {
       colors: {
         'orange-gray': '#917a67',
         'light-brown': '#ede2d1'
-      }
+      },
+      animation: {
+        marquee: 'marquee 40s linear infinite',
+        marquee2: 'marquee2 40s linear infinite',
+      },
+      keyframes: {
+        marquee: {
+          '0%': { transform: 'translateX(0)' },
+          '100%': { transform: 'translateX(-100%)' },
+        },
+        marquee2: {
+          '0%': { transform: 'translateX(100)' },
+          '100%': { transform: 'translateX(0%)' },
+        },
+      },
     },
   },
   plugins: [],
