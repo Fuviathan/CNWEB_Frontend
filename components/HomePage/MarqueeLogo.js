f2ce4441ec238fdb5a93e325fec4faec094d4531
Fu <84614670+Fuviathan@users.noreply.github.com>
2024-02-02 01:21:17 +0700

    done animation

--- components/HomePage/MarqueeLogo.js
+++ components/HomePage/MarqueeLogo.js
@@ -4,26 +4,25 @@ import Marquee from 'react-fast-marquee'
 
 export default function MarqueeLogo() {
   return (
-    <div className='relative flex w-full overflow-x-hidden'>
-      <div className='flex w-full py-12 whitespace-nowrap animate-marquee'>
-        <img src='/brand-01.png' alt='brand' />
-        <img src='/brand-02.png' alt='brand' />
-        <img src='/brand-03.png' alt='brand' />
-        <img src='/brand-04.png' alt='brand' />
-        <img src='/brand-05.png' alt='brand' />
-        <img src='/brand-06.png' alt='brand' />
-        <img src='/brand-07.png' alt='brand' />
-        <img src='/brand-08.png' alt='brand' />
+    <div class="relative flex overflow-x-hidden px-4 ">
+      <div class="flex py-12 animate-marquee whitespace-nowrap">
+        <img src='/brand-01.png' class="mx-4 text-4xl" />
+        <img src='/brand-02.png' class="mx-4 text-4xl" />
+        <img src='/brand-03.png' class="mx-4 text-4xl" />
+        <img src='/brand-04.png' class="mx-4 text-4xl" />
+        <img src='/brand-05.png' class="mx-4 text-4xl" />
+        <img src='/brand-06.png' class="mx-4 text-4xl" />
+        <img src='/brand-07.png' class="mx-6 text-4xl" />
       </div>
-      <div className='flex w-full py-12 whitespace-nowrap animate-marquee2' aria-hidden='true'>
-        <img src='/brand-01.png' alt='brand' />
-        <img src='/brand-02.png' alt='brand' />
-        <img src='/brand-03.png' alt='brand' />
-        <img src='/brand-04.png' alt='brand' />
-        <img src='/brand-05.png' alt='brand' />
-        <img src='/brand-06.png' alt='brand' />
-        <img src='/brand-07.png' alt='brand' />
-        <img src='/brand-08.png' alt='brand' />
+
+      <div class="flex absolute top-0 py-12 animate-marquee2 whitespace-nowrap">
+        <img src='/brand-01.png' class="mx-4 text-4xl" />
+        <img src='/brand-02.png' class="mx-4 text-4xl" />
+        <img src='/brand-03.png' class="mx-4 text-4xl" />
+        <img src='/brand-04.png' class="mx-4 text-4xl" />
+        <img src='/brand-05.png' class="mx-4 text-4xl" />
+        <img src='/brand-06.png' class="mx-4 text-4xl" />
+        <img src='/brand-07.png' class="mx-4 text-4xl" />
       </div>
     </div>
 
