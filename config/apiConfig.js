e1396aa6a5c7b7234bc09722cb87de8b60ffe167
a39006 <vuvantrung002@gmail.com>
2024-02-05 15:56:43 +0700

    config api can't localhost

--- config/apiConfig.js
+++ config/apiConfig.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const API_BASE_URL = "http://localhost:3000/api/";
+export const API_BASE_URL = "https://shopping-9dz1.onrender.com/api/";
 
 // Hàm để lấy token từ localStorage
 const getTokenFromLocalStorage = () => {
