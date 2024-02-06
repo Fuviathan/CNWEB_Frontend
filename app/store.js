adf4065ed04e12c8e57437a74fd41fadc43a4d1a
a39006 <vuvantrung002@gmail.com>
2024-02-06 13:47:41 +0700

    add store for admin

--- app/store.js
+++ app/store.js
@@ -3,6 +3,7 @@ import { configureStore } from "@reduxjs/toolkit";
 import { customerProductReducer } from "@/state/Products/Reducer";
 import { authReducer } from "@/state/Auth/Reducer";
 import { cartReducer } from "@/state/Cart/Reducer";
+import { adminReducer } from "@/state/Admin/Reducer";
 
 // Automatically adds the thunk middleware and the Redux DevTools extension
 export const store = configureStore({
@@ -11,5 +12,6 @@ export const store = configureStore({
     product: customerProductReducer,
     auth: authReducer,
     cart: cartReducer,
+    admin: adminReducer,
   },
 });
