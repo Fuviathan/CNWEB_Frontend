2e9526a834cbdf0f729f79103f07cc9b06eda5a5
Fu <84614670+Fuviathan@users.noreply.github.com>
2024-02-07 18:13:40 +0700

    change language to vietnamese

--- components/HomePage/Carousel/Carousel.js
+++ components/HomePage/Carousel/Carousel.js
@@ -39,7 +39,7 @@ export default function Carousel() {
                     <div className='flex flex-row p-4 h-[30rem]'>
                         <div className='flex flex-col justify-center w-1/3 px-4 rounded-l-lg bg-light-brown'>
                             <div className='font-sans text-3xl font-bold tracking-widest text-right uppercase text-orange-gray '>Laptop</div>
-                            <div className='font-sans text-2xl font-semibold tracking-wide text-right text-orange-gray'>We provide modern laptop</div>
+                            <div className='font-sans text-2xl font-semibold tracking-wide text-right text-orange-gray'>Chúng tôi cung cấp những mẫu laptop hiện đại và tiện lợi nhất</div>
                         </div>
                         <img className='object-cover w-2/3 rounded-r-lg' src='https://mockups-design.com/wp-content/uploads/2022/06/Smartphone_on_White_Background_Mockup_1.jpg'></img>
                     </div>
