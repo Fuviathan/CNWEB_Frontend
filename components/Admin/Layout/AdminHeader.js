e5cd648e7627df19325d840429e308a5378e0112
Fu <84614670+Fuviathan@users.noreply.github.com>
2024-02-07 23:02:38 +0700

    add logout

--- components/Admin/Layout/AdminHeader.js
+++ components/Admin/Layout/AdminHeader.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { UserIcon } from "@heroicons/react/24/outline";
+import { toast } from "react-toastify";
 
 export default function AdminHeader() {
     const [auth, setAuth] = useState();
@@ -12,10 +13,14 @@ export default function AdminHeader() {
         // Get the value from local storage if it exists
         setAuth(value);
     }, []);
-    console.log(auth)
+    function handleLogout() {
+        setAuth("");
+        localStorage.clear();
+        toast.error("Bạn đã đăng xuất");
+    }
     return (
         <div className="flex justify-end w-full px-8 py-2 bg-white shadow">
-            <div className="flex items-center hover:cursor-pointer hover:opacity-70">
+            <div className="flex items-center hover:cursor-pointer hover:opacity-70" onClick={handleLogout}>
                 <UserIcon className="w-6 h-6 font-medium" />
                 <div className="flex flex-col ml-4">
                     <div className="ml-3 text-xl tracking-wide">{auth?.firstname}</div>
