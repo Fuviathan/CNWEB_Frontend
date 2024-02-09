7cd61e8291debae2418819bee2aed1289ea9689d
Fu <84614670+Fuviathan@users.noreply.github.com>
2024-02-09 14:28:01 +0700

    add protect route

--- pages/admin/customer.js
+++ pages/admin/customer.js
@@ -1,17 +1,20 @@
 import AdminLayout from "@/components/Admin/Layout/AdminLayout";
 import ListCustomer from "@/components/Admin/Customer/ListCustomer";
+import ProtectAdminRouter from "@/components/ProtectAdminRouter";
 
 export default function customer() {
   return (
-    <AdminLayout>
-      <div className="w-full max-h-screen mt-2 overflow-auto bg-gray-200">
-        <div className="px-6 pb-4 mt-3 text-2xl font-semibold">
-          Danh sách khách hàng
+    <ProtectAdminRouter>
+      <AdminLayout>
+        <div className="w-full max-h-screen mt-2 overflow-auto bg-gray-200">
+          <div className="px-6 pb-4 mt-3 text-2xl font-semibold">
+            Danh sách khách hàng
+          </div>
+          <div className="px-8 shadow-xl">
+            <ListCustomer></ListCustomer>
+          </div>
         </div>
-        <div className="px-8 shadow-xl">
-          <ListCustomer></ListCustomer>
-        </div>
-      </div>
-    </AdminLayout>
+      </AdminLayout>
+    </ProtectAdminRouter>
   );
 }
--- pages/admin/dashboard.js
+++ pages/admin/dashboard.js
@@ -1,8 +1,11 @@
 import AdminLayout from '@/components/Admin/Layout/AdminLayout'
+import ProtectAdminRouter from '@/components/ProtectAdminRouter'
 import React from 'react'
 
 export default function dashboard() {
   return (
-    <AdminLayout />
+    <ProtectAdminRouter>
+      <AdminLayout />
+    </ProtectAdminRouter>
   )
 }
