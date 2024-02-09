aa68f2d8807b912a4102736eda7ec2e8794c4bc1
Fu <84614670+Fuviathan@users.noreply.github.com>
2024-02-09 14:39:48 +0700

    fix dupplicate file

--- pages/HomePage.js
+++ pages/HomePage.js
@@ -2,7 +2,7 @@ import HomePage from '@/components/HomePage/HomePage'
 import Layout from '@/components/Layout/Layout'
 import React from 'react'
 
-export default function HomePage() {
+export default function homePage() {
     return (
         <Layout >
             <HomePage />    
