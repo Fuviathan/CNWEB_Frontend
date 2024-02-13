fa3f974b68d0ac348def9f77ce93386c8700f709
Fu <84614670+Fuviathan@users.noreply.github.com>
2024-02-13 17:41:36 +0700

    add condition no cart

--- components/Cart/Cart.js
+++ components/Cart/Cart.js
@@ -3,6 +3,7 @@ import CartItem from "./CartItem";
 import PriceDetail from "./PriceDetail";
 import { useDispatch, useSelector } from "react-redux";
 import { getCart } from "@/state/Cart/Action";
+import Link from "next/link";
 
 const Cart = (props) => {
   const [auth, setAuth] = useState();
@@ -14,11 +15,19 @@ const Cart = (props) => {
     // Get the value from local storage if it exists
     value = localStorage.getItem("token") || "";
     setAuth(value);
-    console.log(auth);
     dispatch(getCart());
   }, []);
-
-  return (
+  if (!cartItem) return (
+    <div className="flex flex-col items-center justify-center h-full my-4 bg-white">
+      <div className="p-6 text-2xl font-medium bg-white">
+        Giỏ hàng của bạn chưa có sản phẩm nào
+      </div>
+      <Link href='/product' className="flex justify-center p-3 mx-auto font-sans text-2xl font-semibold rounded-lg shadow-md bg-light-brown w-fit h-fit text-orange-gray hover:opacity-75">
+        Quay lại mua sắm
+      </Link>
+    </div>
+  ) 
+  else if (cart) return (
     <div className="h-full my-4">
       <div className="flex justify-between mx-auto max-w-[1320px]">
         <div className="w-4/6 mr-8">
