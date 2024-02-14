b6aa8d7f0cbb80a8bcbc6be15a51b266ad9d81fa
SooOverpowered <lehoangminh15122000@gmail.com>
2024-02-14 16:07:15 +0700

    add base url env var for deployment

--- config/apiConfig.js
+++ config/apiConfig.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const API_BASE_URL = "http://localhost:3000/api/";
+export const API_BASE_URL = process.env.API_BASE_URL
 
 // Hàm để lấy token từ localStorage
 const getTokenFromLocalStorage = () => {
