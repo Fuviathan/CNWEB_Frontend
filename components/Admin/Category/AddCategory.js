144d98ba68a8e962a49dad28806767a19819d8a0
Fu <84614670+Fuviathan@users.noreply.github.com>
2024-02-17 18:33:35 +0700

    fix image view

--- components/Admin/Category/AddCategory.js
+++ components/Admin/Category/AddCategory.js
@@ -1,12 +1,16 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { addNewCategory } from '@/state/Admin/Action';
 import Dropzone from 'react-dropzone';
-import { uploadImg, deleteImg } from '../../../state/Admin/Action';
+import { uploadImg, handleSetImagesToNull } from '../../../state/Admin/Action';
 import { useForm } from 'react-hook-form'
 import { XMarkIcon } from '@heroicons/react/24/outline';
+import { toast } from "react-toastify";
 
 const AddCategory = (props) => {
+  const dispatch = useDispatch();
+  const images = useSelector((state) => state?.admin?.image);
+  const [img, setImg] = useState([]);
   const {
     register,
     handleSubmit,
@@ -17,15 +21,18 @@ const AddCategory = (props) => {
       title: '',
     }
   })
-  const dispatch = useDispatch();
-  // const schema = yup.object({
-  //   title: yup.string().required('Tên danh mục không được để trống'),
-  //   images: yup.array().required('Phải có ít nhất một ảnh'),
-  // });
-  const images = useSelector((state) => state?.admin?.image);
+  useEffect(() => {
+    if (images) {
+      setImg([...img, ...images]);
+      dispatch(handleSetImagesToNull());
+    }
+  }, [images]);
   const onSubmit = (data) => {
-    data.image = images
-    dispatch(addNewCategory(data))
+    data.image = img
+    console.log(data)
+    if (data.image.length > 1) toast.error('Không được thêm quá 1 ảnh!')
+    else if (data.image.length < 1) toast.error('Không được thiếu ảnh!')
+    else dispatch(addNewCategory(data))
   }
   if (props.open) return (
     <div className='absolute w-3/5 px-10 py-5 mt-4 -translate-x-1/2 -translate-y-1/2 bg-white min-w-fit top-1/2 left-1/2 rounded-xl'>
@@ -34,7 +41,7 @@ const AddCategory = (props) => {
         <label className='block'>Tên danh mục</label>
         <input className='w-full p-2 mt-2 border border-gray-300 rounded-lg' {...register('title', { required: true })} />
         {errors.title && <div className='mt-2 text-sm italic text-red-400 text-italic'>*Không được để trống tên danh mục</div>}
-        {!images ? (
+        {/* {!images ? ( */}
           <div className="p-5 mt-6 text-center bg-white border border-gray-400 rounded-lg cursor-pointer">
             <Dropzone
               onDrop={(acceptedFiles) => {
@@ -53,29 +60,37 @@ const AddCategory = (props) => {
               )}
             </Dropzone>
           </div>
-        ) : (
+          {/* ) : ( */}
           <div className="flex flex-wrap gap-3 mt-3 ">
-            {images?.map((item, index) => {
-              if (item !== 'null') {
+            {img?.map((item, index) => {
+              if (item !== "null") {
                 return (
                   <div key={index} className="relative">
-                    {item?.puclicId ? (
+                    {item?._id || item?.puclicId ? (
                       <button
                         onClick={() => {
-                          dispatch(
-                            deleteImg({id: item.puclicId})
+                          setImg(
+                            img.filter(
+                              (i) =>
+                                i._id !== item._id ||
+                                i.puclicId !== item.puclicId
+                            )
                           );
                         }}
                         className="absolute z-10 cursor-pointer w-10 h-10 top-2.5 right-2.5"
                         type="button"
                       >
-                        <XMarkIcon className='w-6 h-6 text-red-400' />
+                        <XMarkIcon className="w-6 h-6 text-red-400" />
                       </button>
                     ) : (
                       <></>
                     )}
-                    {item?.puclicId ? (
-                      <img src={item?.url} alt="" className='max-w-[300px] min-w-[200px] min-h-[200px] max-h-[300px]' />
+                    {item?._id || item?.puclicId ? (
+                      <img
+                        src={item?.url}
+                        alt=""
+                        className="w-[350px] h-[350px]"
+                      />
                     ) : (
                       <></>
                     )}
@@ -86,7 +101,7 @@ const AddCategory = (props) => {
               }
             })}
           </div>
-        )}
+        {/* )} */}
         <div className="flex flex-row-reverse gap-5 mt-5">
           <button
             type='submit'
