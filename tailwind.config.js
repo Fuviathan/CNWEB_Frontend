9d7eef0ddbf1ddbb7e12bfdf5260944453f17008
Fu <84614670+Fuviathan@users.noreply.github.com>
2024-02-21 23:31:22 +0700

    remove comment

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,7 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  // important: "#root",
+  important: "#root",
   theme: {
     extend: {
       backgroundImage: {
